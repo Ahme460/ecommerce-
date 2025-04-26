bec462e8200cee51187ab2211f2453a6e8cf2b0c
Ahme460 <ahmeoon1234@gmail.com>
2025-04-26 19:02:08 +0300

    model

--- accounts/models.py
+++ accounts/models.py
@@ -49,6 +49,7 @@ class User(AbstractBaseUser , PermissionsMixin, BaseModel):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
+    reset_code=models.PositiveIntegerField(null=True,blank=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     
