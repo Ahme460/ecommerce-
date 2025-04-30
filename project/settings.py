7f999d5dd6401e70543d8c3f036c935946ee751c
Ahme460 <ahmeoon1234@gmail.com>
2025-04-30 19:15:42 +0300

    push

--- project/settings.py
+++ project/settings.py
@@ -176,3 +176,8 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
+
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'  
+SESSION_COOKIE_NAME = "sessionid"   
+SESSION_COOKIE_HTTPONLY = True   
